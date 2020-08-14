0f2137a0034af6920e0101420768300463c7179e
geumilbae <geumil.bae@lanoir42.org>
2020-08-14 15:13:43 +0900

    Writing great tests

--- appointments/src/test/Appointment.test.js
+++ appointments/src/test/Appointment.test.js
@@ -9,24 +9,19 @@ describe('Appointment', () => {
 
     beforeEach(() => {
         container = document.createElement('div');
+        document.body.appendChild(container);
     });
 
     const render = component => ReactDOM.render(component, container);
 
     it('renders the customer first name', () => {
         customer = {firstName: 'Ashley'};
-        document.body.appendChild(container);
-
         render(<Appointment customer={customer}/>);
-
         expect(document.body.textContent).toMatch('Ashley');
     });
     it('renders another customer first name', () => {
         customer = {firstName: 'Jordan'};
-        document.body.appendChild(container);
-
         render(<Appointment customer={customer}/>);
-
         expect(document.body.textContent).toMatch('Jordan');
     });
 });

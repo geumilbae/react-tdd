27d7a79edb3a75f022df7e33e84e30bfd45a013a
geumilbae <geumil.bae@lanoir42.org>
2020-08-14 14:16:52 +0900

    Make it pass

--- appointments/src/test/Appointment.test.js
+++ appointments/src/test/Appointment.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
+import {Appointment} from '../Appointment';
+
 describe('Appointment', () => {
     it('renders the customer first name', () => {
         const customer = {firstName: 'Ashley'};
@@ -11,5 +13,15 @@ describe('Appointment', () => {
         ReactDOM.render(component, container);
 
         expect(document.body.textContent).toMatch('Ashley');
+    });
+    it('renders another customer first name', () => {
+        const customer = {firstName: 'Jordan'};
+        const component = <Appointment customer={customer}/>;
+        const container = document.createElement('div');
+        document.body.appendChild(container);
+
+        ReactDOM.render(component, container);
+
+        expect(document.body.textContent).toMatch('Jordan');
     });
 });
